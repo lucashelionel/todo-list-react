4eaddfd5c7708e9a3de07b000123f58e1072acfb
Lucas Helionel <lucashelionel@gmail.com>
2026-01-06 12:48:58 -0300

    criação do componente ícone e importação da lib que faz isso

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 
 import Text from "./components/text";
+import Icon from "./components/icon";
+import TrashIcon from "./assets/icons/Trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+
 
 export default function App() {
 
   return (
-    <div className="flex flex-col gap-2">
-      <Text variant="body-sm-bold" className="text-pink-base">
-        olá mundo !!
-      </Text>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          olá mundo !!
+        </Text>
 
-            <Text className="text-green-base">
-        olá mundo !!
-      </Text>
+              <Text className="text-green-base">
+          olá mundo !!
+        </Text>
 
-            <Text variant="body-md-bold">
-        olá mundo !!
-      </Text>
+              <Text variant="body-md-bold">
+          olá mundo !!
+        </Text>
 
-            <Text variant="body-sm-bold" className="text-gray-400">
-        olá mundo !!
-      </Text>
+              <Text variant="body-sm-bold" className="text-gray-400">
+          olá mundo !!
+        </Text>
+      </div>
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={PencilIcon} />
+        <Icon svg={XIcon} />
+      </div>
     </div>
-  )
+  );
 }
 
---
+++ src/components/icon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+//variações de animações para os ícones, podendo ter variantes de outros tipos
+export const iconVariants = cva("", {
+    variants:{
+        animate:{
+            false: "",
+            true: "animate-spin"
+        },
+    },
+    defaultVariants:{
+        animate: false
+    }
+});
+
+interface IconProps extends React.ComponentProps<"svg">, VariantProps<typeof iconVariants> {
+    svg: React.FC<React.ComponentProps<"svg">>;
+
+}
+
+export default function Icon({ svg: SvgComponent, animate, className, ...props }: IconProps){
+
+    return <SvgComponent {...props} className={iconVariants({ animate, className })} />;
+}   
