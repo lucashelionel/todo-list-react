90ad7e68ad7ae3ec48ac86cd88ed354f3b41c0c1
Lucas Helionel <lucashelionel@gmail.com>
2026-01-17 22:27:50 -0300

    atualização de badges conforme a criação e exclusão de tarefas

--- src/core-components/tasks-summary.tsx
+++ src/core-components/tasks-summary.tsx
@@ -1,18 +1,24 @@
 import Badge from "../components/badge";
 import Text from "../components/text";
+import useTasks from "../hooks/use-tasks";
 
 export default function TasksSummary() {
-    return <>
-    
-    <div className="flex items-center gap-2">
-    <Text variant="body-sm-bold" className="!text-gray-300">Tarefas criadas</Text>
-    <Badge variant="secondary">5</Badge>  
-    </div>
 
-    <div className="flex items-center gap-2">
-    <Text variant="body-sm-bold" className="!text-gray-300">Concluídas</Text>
-    <Badge variant="primary">2 de 5</Badge>  
-    </div>
+    const {createdTaskCount, concludedTaskCount} = useTasks();
 
-    </>
+    return (
+        <>
+        
+            <div className="flex items-center gap-2">
+                <Text variant="body-sm-bold" className="!text-gray-300">Tarefas criadas</Text>
+                <Badge variant="secondary">{createdTaskCount}</Badge>  
+            </div>
+
+            <div className="flex items-center gap-2">
+                <Text variant="body-sm-bold" className="!text-gray-300">Concluídas</Text>
+                <Badge variant="primary">{concludedTaskCount} de {createdTaskCount}</Badge>  
+            </div>
+
+        </>
+    );
 }
